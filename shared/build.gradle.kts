4bd96da11687a7e74ba758f74bf55903895893df
jming95623 <jming95623@gmail.com>
2024-07-13 16:28:57 +0900

    Add Serializer Into Shared Module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.serializer)
 }
 
 kotlin {
@@ -23,6 +24,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
+            implementation(libs.kotlinx.serialization)
         }
     }
 }
